2f7b6f70cf2a59136e97e91592bfc42d64f837e1
Manfred Wuits <manfred@monochrom.at>
2014-04-08 13:56:36 +0200

    rebuilt for use with ecosystem and BackendBaseControllerForPackages

--- src/Werkzeugh/TranslationAdmin/TranslationAdminController.php
+++ src/Werkzeugh/TranslationAdmin/TranslationAdminController.php
@@ -2,20 +2,9 @@
 
 use BaseController,Redirect, View, Input, Response;
 use Illuminate\Support\Facades\DB;
+use EcoAsset;
 
-
-class TranslationAdminController extends BaseController {
-
-
-  protected $layout = 'backend._layouts.default';
-  protected $layoutdata = ['pageclass'=>'TranslationAdmin'];
-
-
-
-  public function __construct(/*LanguageProvider $languageProvider, LanguageEntryProvider $languageEntryProvider*/)
-  {
-    $this->setProviders(\App::make('Werkzeugh\TranslationAdmin\LanguageProvider'), \App::make('Werkzeugh\TranslationAdmin\LanguageEntryProvider'));
-  }
+class TranslationAdminController extends \BackendBaseControllerForPackages {
 
 
   protected $languageProvider;
@@ -29,35 +18,35 @@ class TranslationAdminController extends BaseController {
 
   public function init()
   {
+      parent::init();
+      $this->setProviders(\App::make('Werkzeugh\TranslationAdmin\LanguageProvider'), \App::make('Werkzeugh\TranslationAdmin\LanguageEntryProvider'));
+  
 
-   \Orchestra\Asset::add('jquery'                    , "/Mwerkzeug/thirdparty/jquery/jquery-1.10.2.min.js");
+       // assume jquery and angular are loaded
+       // EcoAsset::add('jquery'                    , "/Mwerkzeug/thirdparty/jquery/jquery-1.10.2.min.js");
+       // 
+       // EcoAsset::add('angular'                   , "/mysite/thirdparty/ng/angular.min.js");
+       // EcoAsset::add('angular-animate'           , "/mysite/thirdparty/ng/angular-animate.min.js");
+       
+       //TODO remove mysite thirdparty-references
+       EcoAsset::add('tinymce'                   , "/packages/werkzeugh/translation-admin/thirdparty/tinymce4/js/tinymce/tinymce.min.js");
+       EcoAsset::add('ui-tinymce'                , "/packages/werkzeugh/translation-admin/thirdparty/ui-tinymce-0.0.4/src/tinymce.js");
 
-   \Orchestra\Asset::add('angular'                   , "/mysite/thirdparty/ng/angular.min.js");
-   \Orchestra\Asset::add('angular-animate'           , "/mysite/thirdparty/ng/angular-animate.min.js");
+       EcoAsset::add('werkzeugh-translate'       , "/packages/werkzeugh/translation-admin/ng/translation-admin/js/translation-admin.js");
+       EcoAsset::add('werkzeugh-translate-index' , "/packages/werkzeugh/translation-admin/ng/translation-admin/directives/translation-admin-index.js");
+       EcoAsset::add('werkzeugh-translate-item' , "/packages/werkzeugh/translation-admin/ng/translation-admin/directives/translation-admin-item.js");
+       EcoAsset::add('werkzeugh-translate-css'   , "/packages/werkzeugh/translation-admin/css/translation-admin.css");
 
-   \Orchestra\Asset::add('tinymce'                   , "/mysite/thirdparty/tinymce4/js/tinymce/tinymce.min.js");
-   \Orchestra\Asset::add('ui-tinymce'                , "/mysite/thirdparty/ui-tinymce-0.0.4/src/tinymce.js");
-
-   \Orchestra\Asset::add('werkzeugh-translate'       , "/packages/werkzeugh/translation-admin/ng/translation-admin/js/translation-admin.js");
-   \Orchestra\Asset::add('werkzeugh-translate-index' , "/packages/werkzeugh/translation-admin/ng/translation-admin/directives/translation-admin-index.js");
-   \Orchestra\Asset::add('werkzeugh-translate-item' , "/packages/werkzeugh/translation-admin/ng/translation-admin/directives/translation-admin-item.js");
-   \Orchestra\Asset::add('werkzeugh-translate-css'   , "/packages/werkzeugh/translation-admin/css/translation-admin.css");
-
- }
+  }
 
  public function getIndex()
  {
-  $this->init();
 
-        //needed directives:
-        // \Orchestra\Asset::add('wien-import-queue',"/../mysite/ng/wien-import/directives/wien-import-queue.js");
-        // \Orchestra\Asset::add('wien-import-item',"/../mysite/ng/wien-import/directives/wien-import-item.js");
+  
+   $this->eco->setContentTemplate('mypkg::translation-admin-index');
 
-  $this->layout->page_classname='TranslationAdminPage';
-  $this->layout->content = View::make('mypkg::translation-admin-index')->with('data', $data);
-
-
-}
+   return [];
+ }
 
 
 
@@ -325,7 +314,6 @@ public function postNgGroups()
     $model = $this->languageEntryProvider->createModel();
 
     $query=DB::table($model->getTable());
-    $GLOBALS['debugsql']=1;
 
     $lang1_id=$this->getAvailableLanguages()[Input::get('query.lang1').""]['id'];
 
